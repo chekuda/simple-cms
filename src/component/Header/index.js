6993d94ba7fe547dcd4d058abc35a51b24ac11f4
rameshanand <ramesh.anandakrishnan@veiteracitve.com>
2017-09-23 22:27:36 +0100

    build ui based on json

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -2,22 +2,17 @@ import React, { PureComponent } from 'react';
 
 import './style.css';
 
-const header = {
-  backgroundImage:'/img/header-bg.jpg',
-  leadIn:'Welcome To Our Studio!',
-  heading:'It\'s Nice To Meet You',
-  buttonText:'Tell Me More'
-}
-
 export default class Header extends PureComponent {
   render(){
+    const {backgroundImage, leadIn, heading, buttonText} = this.props.component;
+    
     return(
-      <header className="masthead" style={{backgroundImage:`url(${header.backgroundImage})`}}>
+      <header className="masthead" style={{backgroundImage:`url(${backgroundImage})`}}>
         <div className="container">
           <div className="intro-text">
-            <div className="intro-lead-in">{header.leadIn}</div>
-            <div className="intro-heading">{header.heading}</div>
-            <a className="btn btn-xl" href="#services">{header.buttonText}</a>
+            <div className="intro-lead-in">{leadIn}</div>
+            <div className="intro-heading">{heading}</div>
+            <a className="btn btn-xl" href="#services">{buttonText}</a>
           </div>
         </div>
       </header>
